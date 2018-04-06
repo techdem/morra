eabd3b9cb17b543b24af1f99e7f8bb8b7babb881
Tudor Chiribes <tudor.chiribes@gmail.com>
2018-04-07 00:24:46 +0100

    refined formatting

--- Morra.java
+++ Morra.java
@@ -1,67 +1,39 @@
-//import java.util.*;
-//
-//public class Morra {
-//
-//    private int playerSel;
-//    private int playerFingers;
-//    private int computerFingers;
-//
-//    public Morra(int playerSel){
-//
-//        this.playerSel=playerSel;
-//        System.out.println("Player selection is: "+playerSel);
-//    }
-//
-//    public void setFingers(int playerFingers){
-//
-//        this.playerFingers=playerFingers;
-//        System.out.printf("Player has raised %d fingers\n",playerFingers);
-//        computerFingers=new Random().nextInt(9)+1;
-//        System.out.printf("The computer has raised %d fingers\n",computerFingers);
-//    }
-//
-//    public int getComputerFingers(){
-//
-//        return computerFingers;
-//    }
-//
-//    public boolean getResult(){
-//
-//        if(playerSel==0){
-//
-//            return((playerFingers+computerFingers)%2!=0);
-//        }
-//        else{
-//
-//            return((playerFingers+computerFingers)%2==0);
-//        }
-//    }
-//}
-
 import java.util.*;
+
 public class Morra {
-    private int playerSel;
-    private int playerFingers;
-    private int computerFingers;
-    public Morra(int playerSel){
-        this.playerSel=playerSel;
-        System.out.println("Player selection is: "+playerSel);
+
+  private int playerSel;
+  private int playerFingers;
+  private int computerFingers;
+
+  public Morra(int playerSel){
+
+    this.playerSel=playerSel;
+    System.out.println("Player selection is: "+playerSel);
+  }
+
+  public void setFingers(int playerFingers){
+
+    this.playerFingers=playerFingers;
+    System.out.printf("Player has raised %d fingers\n",playerFingers);
+    computerFingers=new Random().nextInt(9)+1;
+    System.out.printf("The computer has raised %d fingers\n",computerFingers);
+  }
+
+  public int getComputerFingers(){
+
+    return computerFingers;
+   }
+
+  public boolean getResult(){
+
+    if(playerSel==0){
+
+      return((playerFingers+computerFingers)%2!=0);
     }
-    public void setFingers(int playerFingers){
-        this.playerFingers=playerFingers;
-        System.out.printf("Player has raised %d fingers\n",playerFingers);
-        computerFingers=new Random().nextInt(9)+1;
-        System.out.printf("The computer has raised %d fingers\n",computerFingers);
+    else{
+
+      return((playerFingers+computerFingers)%2==0);
     }
-    public int getComputerFingers(){
-        return computerFingers;
-    }
-    public boolean getResult(){
-        if(playerSel==0){
-            return((playerFingers+computerFingers)%2!=0);
-        }
-        else{
-            return((playerFingers+computerFingers)%2==0);
-        }
-    }
+  }
 }
