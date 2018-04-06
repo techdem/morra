5be60658eaaf20e3be3a8826be394c2345d29244
Tudor Chiribes <tudor.chiribes@gmail.com>
2018-04-07 00:22:34 +0100

    refined formatting

--- MorraApp.java
+++ MorraApp.java
@@ -1,158 +1,95 @@
-//import javax.swing.*; //import GUI
-//
-//public class MorraApp{
-//
-//	public static void main(String args[]){
-//
-//		JFrame selection=new JFrame();		//frame for selection
-//        String[]yesNo=new String[2];        //array for yes/no
-//		String[]oddsEvens=new String[2];	//array for odds/evens
-//		int playerSel;					    //player selection
-//		int playerFingers=0;				//player fingers
-//        int computerFingers=0;              //computer fingers
-//		int playerScore=0;					//player score
-//		int computerScore=0;				//computer score
-//		int another;						//play another game?
-//        boolean valid=false;                //error catching loop variable
-//
-//        yesNo[0]="Yes";
-//        yesNo[1]="No";
-//
-//		oddsEvens[0]="Odds";
-//		oddsEvens[1]="Evens";
-//
-//		another=JOptionPane.showOptionDialog(selection.getContentPane(),"Welcome to the game of Morra!\n Would you like to play?","The Morra Game",0,JOptionPane.INFORMATION_MESSAGE,null,yesNo,null);
-//
-//		while((another!=JOptionPane.NO_OPTION)){
-//
-//			playerSel=JOptionPane.showOptionDialog(selection.getContentPane(),"What will you choose?","Decisions decisions...",0,JOptionPane.INFORMATION_MESSAGE,null,oddsEvens,null);
-//            Morra myMorra=new Morra(playerSel);	//create new instance of Morra with player selection
-//
-//			while(playerScore<6&&computerScore<6){
-//
-//			    do{
-//                    try{
-//
-//                        playerFingers=Integer.parseInt(JOptionPane.showInputDialog(null,"Choose a number between 1 and 10"));
-//
-//                        if (playerFingers<=0||playerFingers>10) {
-//
-//                            valid=false;
-//                            JOptionPane.showMessageDialog(null,"Invalid input, please try again");
-//                        }
-//                        else{
-//                            valid=true;
-//                        }
-//
-//                    }catch(Exception e){
-//
-//                        valid=false;
-//                        JOptionPane.showMessageDialog(null,"Invalid input, please try again");
-//                    }
-//                } while(!valid);
-//
-//			    myMorra.setFingers(playerFingers);
-//			    computerFingers=myMorra.getComputerFingers();
-//                System.out.println(myMorra.getResult());
-//
-//                if(myMorra.getResult()){
-//                    playerScore+=2;
-//                    JOptionPane.showMessageDialog(null,"The computer has raised "+computerFingers+" fingers!\nYou won this round!");
-//                }
-//                else{
-//                    computerScore+=2;
-//                    JOptionPane.showMessageDialog(null,"The computer has raised "+computerFingers+" fingers!\nYou lost this round!");
-//                }
-//                if(playerFingers>myMorra.getComputerFingers()){
-//                    JOptionPane.showMessageDialog(null,"You get an extra point!");
-//                    playerScore++;
-//                }
-//                else if(playerFingers<myMorra.getComputerFingers()){
-//                    JOptionPane.showMessageDialog(null,"The computer gets an extra point!");
-//                    computerScore++;
-//                }
-//                else{
-//                    JOptionPane.showMessageDialog(null,"Equal number of fingers raised, nobody gets an extra point...");
-//                }
-//                JOptionPane.showMessageDialog(null,"You have " + playerScore + " points and the computer has "+computerScore+" points");
-//                System.out.printf("Player score: %d ; Computer score: %d\n\n",playerScore,computerScore);
-//			}
-//			playerScore=0;
-//			computerScore=0;
-//            another=JOptionPane.showOptionDialog(selection.getContentPane(),"Would you like to play another game?","The End?",0,JOptionPane.INFORMATION_MESSAGE,null,yesNo,null);
-//		}
-//        JOptionPane.showMessageDialog(null,"Good bye!");
-//		System.exit(0);
-//	}
-//}
-
 import javax.swing.*;
+
 public class MorraApp{
+
     public static void main(String args[]){
-        JFrame selection=new JFrame();
-        String[]yesNo=new String[2];
-        String[]oddsEvens=new String[2];
-        int playerSel;
-        int playerFingers=0;
-        int computerFingers;
-        int playerScore=0;
-        int computerScore=0;
-        int another;
-        boolean valid;
-        yesNo[0]="Yes";
-        yesNo[1]="No";
-        oddsEvens[0]="Odds";
-        oddsEvens[1]="Evens";
-        another=JOptionPane.showOptionDialog(selection.getContentPane(),"Welcome to the game of Morra!\n Would you like to play?","The Morra Game",0,JOptionPane.INFORMATION_MESSAGE,null,yesNo,null);
-        while((another!=JOptionPane.NO_OPTION)){
-            playerSel=JOptionPane.showOptionDialog(selection.getContentPane(),"What will you choose?","Decisions decisions...",0,JOptionPane.INFORMATION_MESSAGE,null,oddsEvens,null);
-            Morra myMorra=new Morra(playerSel);
-            while(playerScore<6&&computerScore<6){
-                do{
-                    try{
-                        playerFingers=Integer.parseInt(JOptionPane.showInputDialog(null,"Choose a number between 1 and 10"));
-                        if (playerFingers<=0||playerFingers>10) {
-                            valid=false;
-                            JOptionPane.showMessageDialog(null,"Invalid input, please try again");
-                        }
-                        else{
-                            valid=true;
-                        }
-                    }catch(Exception e){
-                        valid=false;
-                        JOptionPane.showMessageDialog(null,"Invalid input, please try again");
-                    }
-                } while(!valid);
-                myMorra.setFingers(playerFingers);
-                computerFingers=myMorra.getComputerFingers();
-                System.out.println(myMorra.getResult());
-                if(myMorra.getResult()){
-                    playerScore+=2;
-                    JOptionPane.showMessageDialog(null,"The computer has raised "+computerFingers+" fingers!\nYou won this round!");
-                }
-                else{
-                    computerScore+=2;
-                    JOptionPane.showMessageDialog(null,"The computer has raised "+computerFingers+" fingers!\nYou lost this round!");
-                }
-                if(playerFingers>myMorra.getComputerFingers()){
-                    JOptionPane.showMessageDialog(null,"You get an extra point!");
-                    playerScore++;
-                }
-                else if(playerFingers<myMorra.getComputerFingers()){
-                    JOptionPane.showMessageDialog(null,"The computer gets an extra point!");
-                    computerScore++;
-                }
-                else{
-                    JOptionPane.showMessageDialog(null,"Equal number of fingers raised, nobody gets an extra point...");
-                }
-                JOptionPane.showMessageDialog(null,"You have "+playerScore+" points and the computer has "+computerScore+" points");
-                System.out.printf("Player score: %d ; Computer score: %d\n\n",playerScore,computerScore);
-            }
-            playerScore=0;
-            computerScore=0;
-            another=JOptionPane.showOptionDialog(selection.getContentPane(),"Would you like to play another game?","The End?",0,JOptionPane.INFORMATION_MESSAGE,null,yesNo,null);
-        }
-        JOptionPane.showMessageDialog(null,"Good bye!");
-        System.exit(0);
-    }
+
+       JFrame selection=new JFrame();
+       String[]yesNo=new String[2];
+       String[]oddsEvens=new String[2];
+       int playerSel;
+       int playerFingers=0;
+       int computerFingers=0;
+       int playerScore=0;
+       int computerScore=0;
+       int another;
+       boolean valid=false;
+
+       yesNo[0]="Yes";
+       yesNo[1]="No";
+
+       oddsEvens[0]="Odds";
+       oddsEvens[1]="Evens";
+
+       another=JOptionPane.showOptionDialog(selection.getContentPane(),"Welcome to the game of Morra!\n Would you like to play?","The Morra Game",0,JOptionPane.INFORMATION_MESSAGE,null,yesNo,null);
+
+       while((another!=JOptionPane.NO_OPTION)){
+
+           playerSel=JOptionPane.showOptionDialog(selection.getContentPane(),"What will you choose?","Decisions decisions...",0,JOptionPane.INFORMATION_MESSAGE,null,oddsEvens,null);
+           Morra myMorra=new Morra(playerSel);
+
+       while(playerScore<6&&computerScore<6){
+
+           do{
+               try{
+
+                   playerFingers=Integer.parseInt(JOptionPane.showInputDialog(null,"Choose a number between 1 and 10"));
+
+                       if (playerFingers<=0||playerFingers>10) {
+
+                           valid=false;
+                           JOptionPane.showMessageDialog(null,"Invalid input, please try again");
+                       }
+                       else{
+
+                           valid=true;
+                       }
+
+                   }catch(Exception e){
+
+                       valid=false;
+                       JOptionPane.showMessageDialog(null,"Invalid input, please try again");
+                   }
+               } while(!valid);
+
+               myMorra.setFingers(playerFingers);
+               computerFingers=myMorra.getComputerFingers();
+               System.out.println(myMorra.getResult());
+
+               if(myMorra.getResult()){
+
+                   playerScore+=2;
+                   JOptionPane.showMessageDialog(null,"The computer has raised "+computerFingers+" fingers!\nYou won this round!");
+               }
+               else{
+
+                   computerScore+=2;
+                   JOptionPane.showMessageDialog(null,"The computer has raised "+computerFingers+" fingers!\nYou lost this round!");
+               }
+
+               if(playerFingers>myMorra.getComputerFingers()){
+
+                   JOptionPane.showMessageDialog(null,"You get an extra point!");
+                   playerScore++;
+               }
+               else if(playerFingers<myMorra.getComputerFingers()){
+
+                   JOptionPane.showMessageDialog(null,"The computer gets an extra point!");
+                   computerScore++;
+               }
+               else{
+
+                   JOptionPane.showMessageDialog(null,"Equal number of fingers raised, nobody gets an extra point...");
+               }
+
+               JOptionPane.showMessageDialog(null,"You have " + playerScore + " points and the computer has "+computerScore+" points");
+               System.out.printf("Player score: %d ; Computer score: %d\n\n",playerScore,computerScore);
+           }
+           playerScore=0;
+           computerScore=0;
+           another=JOptionPane.showOptionDialog(selection.getContentPane(),"Would you like to play another game?","The End?",0,JOptionPane.INFORMATION_MESSAGE,null,yesNo,null);
+       }
+       JOptionPane.showMessageDialog(null,"Good bye!");
+       System.exit(0);
+   }
 }
